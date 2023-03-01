a49c9191ed08b4d26b7b793ef49dc1d333ebf682
Anipollak <anipollak1122@gmail.com>
2023-02-28 23:40:45 -0300

    cv argentina programa commit

---
+++ funciones.js
@@ -0,0 +1,23 @@
+document.getElementById('boton').onclick = function(){ 
+console.log('3516854575');
+document.getElementById('contactame').innerHTML = "Aca me podes encontrar: 3515454545";
+
+}
+
+document.getElementById('boton-referencia1').onclick = function(){ 
+    console.log('3516854575');
+    document.getElementById('referencia1').innerHTML = "351111111";
+    
+    }
+
+    document.getElementById('boton-referencia2').onclick = function(){ 
+        console.log('3516854575');
+         document.getElementById('referencia2').innerHTML = "352222222";
+            
+         }
+    
+     document.getElementById('boton-referencia3').onclick = function(){ 
+         console.log('3516854575');
+        document.getElementById('referencia3').innerHTML = "353333333";
+                
+         }
